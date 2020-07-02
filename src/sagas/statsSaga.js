722e87bb585bff6570a268143d1e8943ef943b5a
vaibhavAccionLabs <vaibhav.katariya@accionlabs.com>
2020-07-02 17:56:35 +0530

    Initial saga app setup

---
+++ src/sagas/statsSaga.js
@@ -0,0 +1,34 @@
+import { take, call, fork, put } from 'redux-saga/effects';
+
+import { IMAGES } from '../constants';
+import { fetchImageStats } from '../api';
+import { loadImageStats, setImageStats, setImageStatsError } from '../actions';
+
+export function* handleStatsRequest(id) {
+    for (let i = 0; i < 3; i++) {
+        try {
+            yield put(loadImageStats(id));
+            const res = yield call(fetchImageStats, id);
+            yield put(setImageStats(id, res.downloads.total));
+            // image was loaded so we exit the generator
+            return true;
+        } catch (e) {
+            // we just need to retry and dispactch an error
+            // if we tried more than 3 times
+        }
+    }
+    yield put(setImageStatsError(id));
+}
+
+export default function* watchStatsRequest() {
+    while (true) {
+        // we get the action here
+        const data = yield take(IMAGES.LOAD_SUCCESS);
+        console.log(data);
+        const { images } = data;
+
+        for (let i = 0; i < images.length; i++) {
+            yield fork(handleStatsRequest, images[i].id);
+        }
+    }
+}
